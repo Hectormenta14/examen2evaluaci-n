2798ade8df34335edf8d100efd825487f5fa5f41
Hectormenta14 <webhector14@gmail.com>
2024-06-02 23:19:03 +0200

    dsf

---
+++ Examen2Eval (alumns)/Assets/yanes/Goblin.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Goblin : MonoBehaviour
+{
+    //constructor        herencia que recibe de la clase Character
+    public Goblin() : base("Goblin", 10, Resources.Load<Sprite>("Sprites/goblin"))
+    {
+    }
+    public override float Attack()      // Metodo Attack que recibe de Character
+    {
+        if (health <= 20)
+        {
+            damage *= 3;
+        }
+        return damage;
+    }
+
+    public override float Heal()        // Metodo Heal que recibe de Character 
+    {
+        health += damage * 0.5f;
+        base.Heal();
+        return health;
+    }
+}
