07757be2ccc526a8ac750d41050be30ea9a1b2f4
Hectormenta14 <webhector14@gmail.com>
2024-06-03 02:07:35 +0200

    fin

--- Examen2Eval (alumns)/Assets/examen/PlayerMovement.cs
+++ Examen2Eval (alumns)/Assets/examen/PlayerMovement.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    private int jumpcount = 0, mamaMiacount;
+    private TMP_Text jumpText, mamaMiaText;
+    private Animator animator;
     void Update()
     {
-        
+        if(x != 0)
+        {
+            animator.SetBool("iswalking", true);
+
+        }
+        else
+        {
+            animator.SetBool("iswalking", false);
+        }
     }
+    void Start()
+    {
+        jumpText = UImanager.GetInstance().GetUIElemet("jumpstext").GetComponent<TMP_Text>();
+        mamaMiaText = UImanager.GetInstance().GetUIElemet("stepstext").GetComponent<TMP_Text>();
+
+    }
+    public void IncrementMamaMia()
+    {
+        mamaMiacount++;
+        mamaMiaText.text = "mama mia" + mamaMiacount;
+    }
+    //parte del salto
+    jumpcount ++;
+    jumpText.text = "Numero de saltos:" + jumpcount;
 }
